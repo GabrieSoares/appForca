a548f7cfcdd9f907ebefed44b9cb0324c3b017ab
Gabriel Soares <gabriel.mello2016@gmail.com>
2019-10-14 23:04:31 -0300

    Incluso funções iniciais do app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  palavras = ['gremio', 'flamengo', 'inter', 'palmeiras', 'paralelepipedo'];
+  palavraSorteada = [];
+  resposta = [];
+  usadas = "";
+  letra: string;
+  erros = 0;
 
-  constructor() {}
 
+  constructor() { }
+  sortear() {
+    this.palavraSorteada = this.palavras[this.aleatorio()].split("");
+    this.resposta = new Array(this.palavraSorteada.length);
+    console.log(this.palavraSorteada);
+    console.log(this.resposta);
+  }
+  aleatorio() {
+    return Math.floor(Math.random() * (this.palavras.length - 0) + 0);
+  }
+  validar(letra: string) {
+    let existe = false;
+    this.usadas = this.usadas + letra;
+    
+    for (let i = 0; i < this.palavraSorteada.length; i++) {
+      console.log(letra);
+      if (this.palavraSorteada[i] === letra) {
+        this.resposta[i] = letra;
+        existe = true;
+      }
+    }
+    this.resposta.filter(elemento => {
+      if (elemento == this.palavraSorteada[elemento]) {
+        console.log(elemento);
+      }
+    });
+    if (!existe) {
+      this.erros++;
+    }
+    this.letra = "";
+  }
 }
