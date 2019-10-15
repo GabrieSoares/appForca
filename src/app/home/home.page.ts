c401278d2b382edc8b09749b3739e802bb7c2285
Gabriel Soares <gabriel.mello2016@gmail.com>
2019-10-15 00:23:52 -0300

    Incrementada a função para verificar o fim do jogo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -6,20 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  palavras = ['gremio', 'flamengo', 'inter', 'palmeiras', 'paralelepipedo'];
+  palavras = ['gremio', 'flamengo', 'inter', 'bahia', 'flu'];
   palavraSorteada = [];
+  tamanho: number;
   resposta = [];
   usadas = "";
   letra: string;
   erros = 0;
 
 
-  constructor() { }
+  constructor(public alertController: AlertController, ) { }
   sortear() {
     this.palavraSorteada = this.palavras[this.aleatorio()].split("");
-    this.resposta = new Array(this.palavraSorteada.length);
+    this.tamanho = this.palavraSorteada.length;
+    this.resposta = new Array(this.tamanho);
     console.log(this.palavraSorteada);
-    console.log(this.resposta);
   }
   aleatorio() {
     return Math.floor(Math.random() * (this.palavras.length - 0) + 0);
@@ -27,22 +30,41 @@ export class HomePage {
   validar(letra: string) {
     let existe = false;
     this.usadas = this.usadas + letra;
-    
+
     for (let i = 0; i < this.palavraSorteada.length; i++) {
-      console.log(letra);
       if (this.palavraSorteada[i] === letra) {
         this.resposta[i] = letra;
         existe = true;
       }
     }
-    this.resposta.filter(elemento => {
-      if (elemento == this.palavraSorteada[elemento]) {
-        console.log(elemento);
-      }
-    });
     if (!existe) {
       this.erros++;
     }
+    if (this.fimjogo(this.palavraSorteada, this.resposta)) {
+      this.alertaVitoria();
+    }
     this.letra = "";
+  }
+  fimjogo(palavraSorteada, resposta) {
+    let fim = 0;
+    for (let i = 0; i < resposta.length; i++) {
+      if (palavraSorteada[i] === resposta[i]) {
+        fim++;
+      }
+      if (fim === resposta.length) {
+        return true;
+      }
+    }
+    return false;
+  }
+  async alertaVitoria() {
+    const alert = await this.alertController.create({
+      header: 'Parabéns!',
+      subHeader: 'Fim de Jogo',
+      message: "você acertou a palavra",
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
